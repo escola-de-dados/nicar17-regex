12632584170a55c298aeded6ea6952af309edd20
Marco Túlio Pires <marco@bitcount.com.br>
2017-03-30 23:16:29 -0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-(function(global, document, BodySwitcher, Tester) {
+d(function(global, document, BodySwitcher, Tester) {
 'use strict';
 
 var bodiesForm = document.getElementById('bodies'),
@@ -11,9 +11,9 @@ tester = new Tester(testerForm);
 
 // Load pieces of sample text and connect them to the environment.
 bodySwitcher = new BodySwitcher(bodiesForm, tester, {
-  'College URLs': 'bodies/college-urls.txt',
-  'EFF wordlist': 'bodies/eff-wordlist.txt',
-  'People': 'bodies/people.txt'
+  'Universidades': 'bodies/universidades.txt',
+  'Hospitais SP': 'bodies/hospitais_SP.txt',
+  'Pessoas dos EUA': 'bodies/people.txt'
 });
 
 // FIXME: Remove these testing variables.
